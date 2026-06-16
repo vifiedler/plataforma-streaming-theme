f579505b0118dc64a17accf4c2ac87e69936676a
Vicente Fiedler Montenegro <vi.fiedler@duocuc.cl>
2026-06-16 14:48:26 -0400

    Update content-single.php

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,7 +11,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('mb-5'); ?>>
 
     <?php
-    // ===== Video como hero (arriba de todo) =====
+    // video hero, sacado de acf
     $iframe = get_field('url_video');
 
     if (!empty($iframe)):
