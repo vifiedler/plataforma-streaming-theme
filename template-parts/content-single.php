aed4c63016e207a281c6cef573c42fefbcf3824a
Vicente Fiedler Montenegro <vi.fiedler@duocuc.cl>
2026-06-16 13:19:11 -0400

    probando oembed
    se agrego codigo oembed de acf, pra probar si funciona o no

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,8 +11,33 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('mb-5'); ?>>
 
     <div class="single-img-container mb-3">
-        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="<?php echo get_the_title(); ?>">
+        <img src="<?php echo esc_url(get_field('imagen_video')['url']); ?>" alt="<?php echo get_the_title(); ?>">
     </div>
+    <?php
+
+// Load value.
+$iframe = get_field('oembed');
+
+// Use preg_match to find iframe src.
+preg_match('/src="(.+?)"/', $iframe, $matches);
+$src = $matches[1];
+
+// Add extra parameters to src and replace HTML.
+$params = array(
+    'controls'  => 0,
+    'hd'        => 1,
+    'autohide'  => 1
+);
+$new_src = add_query_arg($params, $src);
+$iframe = str_replace($src, $new_src, $iframe);
+
+// Add extra attributes to iframe HTML.
+$attributes = 'frameborder="0"';
+$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+// Display customized HTML.
+echo $iframe;
+?>
 
     <div class="px-3 px-md-0">
 
@@ -28,7 +53,7 @@
             $avatar_url = get_avatar_url($autor_id, ['size' => 80]);
             if ($avatar_url):
                 ?>
-                <img src="<?php echo $avatar_url; ?>" alt="<?php echo get_the_author(); ?>">
+            <img src="<?php echo $avatar_url; ?>" alt="<?php echo get_the_author(); ?>">
             <?php endif; ?>
             <div>
                 <div class="single-byline-name">
