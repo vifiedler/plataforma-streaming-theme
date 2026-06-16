c59804eb37a9ac6e00e0e57fdc8a492a61831768
vifiedler <vi.fiedler@duocuc.cl>
2026-06-15 21:48:39 -0400

    Agregó modulos al assets

--- assets/assets.php
+++ assets/assets.php
@@ -4,3 +4,4 @@ include get_template_directory() . '/assets/includes/css-functions.php';
 include get_template_directory() . '/assets/includes/js-functions.php';
 include get_template_directory() . '/assets/includes/menu-functions.php';
 include get_template_directory() . '/assets/includes/widgets-functions.php';
+include get_template_directory() . '/assets/includes/modulos-functions.php';
