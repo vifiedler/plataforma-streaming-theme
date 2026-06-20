881773a641ede10f840409163a8c776811d0c54c
vifiedler <vi.fiedler@duocuc.cl>
2026-06-20 13:11:05 -0400

    Update content-search.php

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -16,20 +16,20 @@
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-			nota2_template_posted_on();
-			nota2_template_posted_by();
+			nota3_template_posted_on();
+			nota3_template_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php nota2_template_post_thumbnail(); ?>
+	<?php nota3_template_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php nota2_template_entry_footer(); ?>
+		<?php nota3_template_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
