6dcdcdb6d3244bcb13bc4b5092119d0653c69fb7
vifiedler <vi.fiedler@duocuc.cl>
2026-06-20 13:43:51 -0400

    Cambios en functions.

--- assets/templates/navs/nav-desk.php
+++ assets/templates/navs/nav-desk.php
@@ -1,18 +1,19 @@
 <nav class="navbar navbar-expand-lg fbs__net-navbar">
   <div class="container">
     <?php the_custom_logo(); ?>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <?php
       wp_nav_menu(array(
-          'theme_location' => 'menu-superior',
-          'menu_class'     => 'navbar-nav mb-2 mb-lg-0',
-          'container'      => 'false',
-          'depth'          => 2,
-          'walker'         => new bootstrap_5_wp_nav_menu_walker(),
-          'fallback_cb'    => 'bootstrap_5_wp_nav_menu_walker::fallback',
+        'theme_location' => 'menu-superior',
+        'menu_class' => 'navbar-nav mb-2 mb-lg-0',
+        'container' => 'false',
+        'depth' => 2,
+        'walker' => new bootstrap_5_wp_nav_menu_walker(),
+        'fallback_cb' => 'bootstrap_5_wp_nav_menu_walker::fallback',
       ));
       ?>
     </div>
