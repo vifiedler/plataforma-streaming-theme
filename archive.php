b97ff6ebd9fad330c54488827b798dbdaadc4135
vifiedler <vi.fiedler@duocuc.cl>
2026-06-22 19:43:23 -0400

    Update archive.php

--- archive.php
+++ archive.php
@@ -14,7 +14,7 @@ get_header();
 
 	<?php if (have_posts()): ?>
 
-		<header class="page-header container-fluid">
+		<header class="page-header container-fluid mt-5">
 			<?php
 			the_archive_title('<h1 class="page-title">', '</h1>');
 			the_archive_description('<div class="archive-description">', '</div>');
