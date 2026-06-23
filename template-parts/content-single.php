48421b24e9a986793ee4e408f042f1019676a4d2
vifiedler <vi.fiedler@duocuc.cl>
2026-06-22 23:32:33 -0400

    Update content-single.php

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -46,7 +46,7 @@
                 </div>
             </div>
         </div>
-        <div class="mt-4 pt-3 links-single">
+        <div class="mt-4 pt-3 links-single">ACA O NO?
             <?php nota3_template_entry_footer(); ?>
         </div>
     </div>
