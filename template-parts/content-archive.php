3f8beb62a7375a2ea896029f5005513c35d76f85
Vicente Fiedler Montenegro <vi.fiedler@duocuc.cl>
2026-06-23 12:49:33 -0400

    Update content-archive.php

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -6,25 +6,23 @@
  */
 ?>
 
-<div class="col-12 col-sm-6 col-md-4 col-lg-3">
-	<div class="video-card">
+<div class="col-12 col-md-4">
+    <div class="video-card">
+        <a href="<?php the_permalink(); ?>" class="d-block text-decoration-none">
+            <div class="video-card__thumb-wrap">
+                <img src="<?php echo esc_url(get_field('imagen_video')['url']); ?>"
+                    alt="<?php echo esc_attr(get_the_title()); ?>" class="video-card__thumb">
+                <span class="video-card__duration"><?php echo get_field('duracion');?></span>
+            </div>
+        </a>
+        <div class="video-card__body">
+            <h3 class="video-card__title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+            <p class="video-card__artist">
+                <?php echo esc_html(get_field('nombre_artista')); ?>
+            </p>
+        </div>
 
-		<a href="<?php the_permalink(); ?>" class="d-block text-decoration-none">
-			<div class="video-card__thumb-wrap">
-				<img src="<?php echo esc_url(get_field('imagen_video')['url']); ?>"
-					alt="<?php echo esc_attr(get_the_title()); ?>" class="video-card__thumb">
-
-			</div>
-		</a>
-
-		<div class="video-card__body">
-			<h3 class="video-card__title">
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</h3>
-			<p class="video-card__artist">
-				<?php echo esc_html(get_field('nombre_artista')); ?>
-			</p>
-		</div>
-
-	</div>
+    </div>
 </div>
