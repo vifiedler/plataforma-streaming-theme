170fbeff5ffcf3e81f478ea8113538dd6666796d
vifiedler <vi.fiedler@duocuc.cl>
2026-06-22 22:54:00 -0400

    cambios en barra de busqueda

--- assets/templates/navs/nav-desk.php
+++ assets/templates/navs/nav-desk.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg fbs__net-navbar">
-    <?php the_custom_logo(); ?>
-    <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <?php
+<nav class="navbar navbar-expand-lg fbs__net-navbar d-flex align-items-center">
+  <?php the_custom_logo(); ?>
+  <div class="container d-flex align-items-center justify-content-between">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <?php
       wp_nav_menu(array(
         'theme_location' => 'menu-superior',
         'menu_class' => 'navbar-nav mb-2 mb-lg-0',
@@ -16,9 +16,10 @@
         'fallback_cb' => 'bootstrap_5_wp_nav_menu_walker::fallback',
       ));
       ?>
-        </div>
-            <div class="nav-search-wrap">
-        <?php get_search_form(); ?>
     </div>
-    </div>
+
+  </div>
+  <div class="nav-search-wrap ms-auto me-3">
+    <?php get_search_form(); ?>
+  </div>
 </nav>
