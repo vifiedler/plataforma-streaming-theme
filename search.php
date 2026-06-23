82665c17a75b9f5326aae457a61f5337727815ac
vifiedler <vi.fiedler@duocuc.cl>
2026-06-23 01:31:20 -0400

    Update search.php

--- search.php
+++ search.php
@@ -10,31 +10,33 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-    <?php if (have_posts()) : ?>
-        <header class="page-header mt-5">
-            <h1 class="page-title">
-                <?php
-                printf(
-                    esc_html__('Resultados de búsqueda para: %s', 'nota3-template'),
-                    '<span>' . get_search_query() . '</span>'
-                );
-                ?>
-            </h1>
-        </header><!-- .page-header -->
+	<?php if (have_posts()): ?>
+		<header class="page-header mt-5">
+			<h1 class="page-title">
+				<?php
+				printf(
+					esc_html__('Resultados de búsqueda para: %s', 'nota3-template'),
+					'<span>' . get_search_query() . '</span>'
+				);
+				?>
+			</h1>
+		</header><!-- .page-header -->
 
-        <div class="container-fluid row g-4">
-            <?php while (have_posts()) : the_post(); ?>
-                <?php get_template_part('template-parts/content', 'videos'); ?>
-            <?php endwhile; ?>
-        </div>
+		<div class="container-fluid row g-4">
+			<?php while (have_posts()):
+				the_post(); ?>
+				<?php get_template_part('template-parts/content', 'videos'); ?>
+			<?php endwhile; ?>
+			<?php the_posts_navigation(); ?>
+		</div>
 
-        <?php the_posts_navigation(); ?>
 
-    <?php else : ?>
-        <?php get_template_part('template-parts/content', 'none'); ?>
-    <?php endif; ?>
+
+	<?php else: ?>
+		<?php get_template_part('template-parts/content', 'none'); ?>
+	<?php endif; ?>
 
 </main><!-- #main -->
 
