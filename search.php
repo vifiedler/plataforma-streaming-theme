70cd2c789fb7fc097befe00732866f0705cb12ba
vifiedler <vi.fiedler@duocuc.cl>
2026-06-22 22:47:26 -0400

    Update search.php

--- search.php
+++ search.php
@@ -12,41 +12,31 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+    <?php if (have_posts()) : ?>
+        <header class="page-header">
+            <h1 class="page-title">
+                <?php
+                printf(
+                    esc_html__('Resultados de búsqueda para: %s', 'nota3-template'),
+                    '<span>' . get_search_query() . '</span>'
+                );
+                ?>
+            </h1>
+        </header><!-- .page-header -->
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'nota3-template' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+        <div class="row g-4">
+            <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('template-parts/content', 'videos'); ?>
+            <?php endwhile; ?>
+        </div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+        <?php the_posts_navigation(); ?>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+    <?php else : ?>
+        <?php get_template_part('template-parts/content', 'none'); ?>
+    <?php endif; ?>
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
