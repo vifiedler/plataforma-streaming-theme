1be927f12a699b20c4d8353bf2889e5543009060
vifiedler <vi.fiedler@duocuc.cl>
2026-06-23 01:30:38 -0400

    Update search.php

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ get_header();
 	<main id="primary" class="site-main">
 
     <?php if (have_posts()) : ?>
-        <header class="page-header">
+        <header class="page-header mt-5">
             <h1 class="page-title">
                 <?php
                 printf(
