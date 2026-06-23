325a6f8b65989e3ba528a4b77c690c0ebc24d871
vifiedler <vi.fiedler@duocuc.cl>
2026-06-23 01:19:22 -0400

    Update single.php

--- single.php
+++ single.php
@@ -10,14 +10,14 @@
 get_header();
 ?>
 
-<main id="primary" class="">
+<main id="primary" class="site-main">
 
     <?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content-single', get_post_type() );?>
-    <div class=""><?php
+    <div class="container-fluid my-3 px-5"><?php
 			the_post_navigation(
 				array(
 					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'nota3-template' ) . '</span> <span class="nav-title">%title</span>',
