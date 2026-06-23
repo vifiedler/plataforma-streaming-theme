f200bd98cba2f0d4516d205af0727257fb7da549
Vicente Fiedler Montenegro <vi.fiedler@duocuc.cl>
2026-06-23 14:04:58 -0400

    Update archive.php

--- archive.php
+++ archive.php
@@ -17,7 +17,6 @@ get_header();
     <header class="page-header container-fluid mt-5">
         <?php
 			the_archive_title('<h1 class="page-title">', '</h1>');
-			the_archive_description('<div class="archive-description">', '</div>');
 			?>
     </header><!-- .page-header -->
     <!-- Sección Destacados -->
