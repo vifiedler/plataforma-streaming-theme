dff7dc996c62ca356ca012bf6b3a8b9bfe29d8e1
vifiedler <vi.fiedler@duocuc.cl>
2026-06-22 23:15:01 -0400

    Update nav-desk.php

--- assets/templates/navs/nav-desk.php
+++ assets/templates/navs/nav-desk.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg fbs__net-navbar">
-    <div class="container-fluid d-flex flex-wrap align-items-center">
-        <div class="navbar-brand-wrapper col-12 col-lg-auto">
+    <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap">
+        <div class="navbar-brand-wrap">
             <?php the_custom_logo(); ?>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
@@ -18,9 +18,9 @@
                 'fallback_cb' => 'bootstrap_5_wp_nav_menu_walker::fallback',
             ));
             ?>
-        </div>
-        <div class="nav-search-wrap ms-lg-auto mt-2 mt-lg-0">
-            <?php get_search_form(); ?>
+            <div class="nav-search-wrap ms-lg-auto">
+                <?php get_search_form(); ?>
+            </div>
         </div>
     </div>
 </nav>
