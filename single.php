3c2edb1e2768dde5f4e5cf13d15c113255e47c84
Vicente Fiedler Montenegro <vi.fiedler@duocuc.cl>
2026-06-24 14:42:36 -0400

    Update single.php

--- single.php
+++ single.php
@@ -26,16 +26,19 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
-	<section class="container-fluid"><h2 class="ms-3"><?php echo esc_html($current_term->name);?></h2>
-<?php include get_template_directory() . '/assets/modulos/modulo-video/loop-mp-carrusel-single.php'; ?></section>
+    <section class="container-fluid">
+        <h2 class="ms-3"><?php echo esc_html($current_term->name);?></h2>
+        <?php include get_template_directory() . '/assets/modulos/modulo-video/loop-mp-carrusel-single.php'; ?>
+    </section>
     <div class="container-fluid my-3 px-5">
         <div class="row mb-5">
             <!--imagen artista-->
-            <div class="col-md-4">
+            <div class="col-md-8">
                 <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>"
                     alt="<?php echo get_the_title(); ?>" class="img-fluid rounded">
+                <p><?php echo get_field('descripcion'); ?></p>
             </div><!-- Descripción del video -->
-            <div class="col-md-8">
+            <div class="col-md-4">
                 <p><?php echo get_field('descripcion'); ?></p>
             </div>
         </div> <!-- Géneros -->
