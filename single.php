76d984e1fcb41aa30f2c51a665d2c7fd1e8bf0d8
Vicente Fiedler Montenegro <vi.fiedler@duocuc.cl>
2026-06-24 14:40:20 -0400

    Update single.php

--- single.php
+++ single.php
@@ -28,6 +28,33 @@ get_header();
 		?>
 	<section class="container-fluid"><h2 class="ms-3"><?php echo esc_html($current_term->name);?></h2>
 <?php include get_template_directory() . '/assets/modulos/modulo-video/loop-mp-carrusel-single.php'; ?></section>
+    <div class="container-fluid my-3 px-5">
+        <div class="row mb-5">
+            <!--imagen artista-->
+            <div class="col-md-4">
+                <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>"
+                    alt="<?php echo get_the_title(); ?>" class="img-fluid rounded">
+            </div><!-- Descripción del video -->
+            <div class="col-md-8">
+                <p><?php echo get_field('descripcion'); ?></p>
+            </div>
+        </div> <!-- Géneros -->
+        <?php
+                $generos = get_the_terms(get_the_ID(), 'genero_videos');
+                if (!empty($generos) && !is_wp_error($generos)):
+                    ?>
+        <div class="mb-2">
+            <?php foreach ($generos as $genero): ?>
+            <a href="<?php echo esc_url(get_term_link($genero)); ?>" class="tm-hero-btn">
+                <?php echo esc_html($genero->name); ?>
+            </a>
+            <?php endforeach; ?>
+        </div>
+        <?php endif; ?>
+        <div class="mt-4 pt-3 links-single">
+            <?php nota3_template_entry_footer(); ?>
+        </div>
+    </div>
 </main><!-- #main -->
 
 <?php
