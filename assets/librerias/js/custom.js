81270cc55f1333e507ea8619811923eeca64da36
vifiedler <vi.fiedler@duocuc.cl>
2026-06-29 11:18:24 -0400

    Update custom.js

--- assets/librerias/js/custom.js
+++ assets/librerias/js/custom.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-    /* ===== Carruseles horizontales ===== */
+    /* Carruseles horizontales */
     let scrollAmount = 680;
 
     $('.bd-carousel-next').on('click', function () {
@@ -13,10 +13,19 @@
         $track.animate({ scrollLeft: $track.scrollLeft() - scrollAmount }, 350);
     });
 
-    /* ===== Single: play toggle ===== */
+    /* Single: play toggle con el autoplay */
     $('#bd-play-btn').on('click', function () {
         $('#bd-single-hero').addClass('d-none');
         $('#bd-single-video').removeClass('d-none');
+        let $iframe = $('#video-container').find('iframe');
+        if ($iframe.length){
+            let src = $iframe.attr('src');
+            if(src.indexOf('autoplay=1')===-1){
+                let separator = src.indexOf('?') !== -1 ? '&' : '?';
+                let newSrc = src + separator + 'autoplay=1';
+                $iframe.attr('src',newSrc);
+            }
+        }
     });
 
 })(jQuery);
