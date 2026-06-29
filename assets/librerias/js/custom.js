7afe82c402bb589e03668ad3dd4ec2ea54b3b950
vifiedler <vi.fiedler@duocuc.cl>
2026-06-29 11:43:48 -0400

    Update custom.js

--- assets/librerias/js/custom.js
+++ assets/librerias/js/custom.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-    /* Carruseles horizontales */
+    /* ===== Carruseles horizontales ===== */
     let scrollAmount = 680;
 
     $('.bd-carousel-next').on('click', function () {
@@ -13,18 +13,77 @@
         $track.animate({ scrollLeft: $track.scrollLeft() - scrollAmount }, 350);
     });
 
-    /* Single: play toggle con el autoplay */
-    $('#bd-play-btn').on('click', function () {
-        $('#bd-single-hero').addClass('d-none');
-        $('#bd-single-video').removeClass('d-none');
-        let $iframe = $('#video-container').find('iframe');
-        if ($iframe.length){
-            let src = $iframe.attr('src');
-            if(src.indexOf('autoplay=1')===-1){
-                let separator = src.indexOf('?') !== -1 ? '&' : '?';
-                let newSrc = src + separator + 'autoplay=1';
-                $iframe.attr('src',newSrc);
+    /* ===== Single: control de reproducción con pausa ===== */
+    var $playBtn = $('#bd-play-btn');
+    var $pauseBtn = $('#bd-pause-btn');
+    var $videoContainer = $('#bd-single-video');
+    var $overlay = $('#bd-overlay');
+    var $iframe = null;
+    var isPaused = false;
+
+    // Función para enviar comandos al reproductor de YouTube
+    function sendCommand(command) {
+        if (!$iframe) return;
+        var message = JSON.stringify({
+            event: 'command',
+            func: command,
+            args: ''
+        });
+        $iframe[0].contentWindow.postMessage(message, '*');
+    }
+
+    // Obtener el iframe y configurar enablejsapi=1 + autoplay
+    function getIframe() {
+        if (!$iframe) {
+            $iframe = $('#video-container').find('iframe');
+            if ($iframe.length) {
+                var src = $iframe.attr('src');
+                if (src.indexOf('enablejsapi=1') === -1) {
+                    var separator = src.indexOf('?') !== -1 ? '&' : '?';
+                    var newSrc = src + separator + 'enablejsapi=1';
+                    if (newSrc.indexOf('autoplay=1') === -1) {
+                        newSrc += '&autoplay=1';
+                    }
+                    $iframe.attr('src', newSrc);
+                } else {
+                    if (src.indexOf('autoplay=1') === -1) {
+                        $iframe.attr('src', src + '&autoplay=1');
+                    }
+                }
             }
+        }
+        return $iframe;
+    }
+
+    // Botón Reproducir
+    $playBtn.on('click', function () {
+        // Mostrar el video
+        $videoContainer.show();
+        // Obtener y configurar el iframe
+        var iframe = getIframe();
+        if (iframe && iframe.length) {
+            if (isPaused) {
+                // Si estaba pausado, reanudar
+                sendCommand('playVideo');
+                isPaused = false;
+                $playBtn.hide();
+                $pauseBtn.show();
+            } else {
+                // Primera reproducción
+                $playBtn.hide();
+                $pauseBtn.show();
+                $overlay.addClass('video-active');
+            }
+        }
+    });
+
+    // Botón Pausa
+    $pauseBtn.on('click', function () {
+        if ($iframe && $iframe.length) {
+            sendCommand('pauseVideo');
+            isPaused = true;
+            $pauseBtn.hide();
+            $playBtn.show();
         }
     });
 
