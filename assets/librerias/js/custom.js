5ee96c68f410da9e8c7ee51cc2e342a8c66c58c9
vifiedler <vi.fiedler@duocuc.cl>
2026-06-29 11:09:11 -0400

    Update custom.js

--- assets/librerias/js/custom.js
+++ assets/librerias/js/custom.js
@@ -1,5 +1,6 @@
 (function ($) {
 
+    /* ===== Carruseles horizontales ===== */
     let scrollAmount = 680;
 
     $('.bd-carousel-next').on('click', function () {
@@ -10,6 +11,12 @@
     $('.bd-carousel-prev').on('click', function () {
         let $track = $(this).siblings('.bd-carousel-track');
         $track.animate({ scrollLeft: $track.scrollLeft() - scrollAmount }, 350);
+    });
+
+    /* ===== Single: play toggle ===== */
+    $('#bd-play-btn').on('click', function () {
+        $('#bd-single-hero').addClass('d-none');
+        $('#bd-single-video').removeClass('d-none');
     });
 
 })(jQuery);
