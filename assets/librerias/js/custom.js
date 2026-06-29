6923ba920be06f9e772f1cf2d1074354ef0e8a99
vifiedler <vi.fiedler@duocuc.cl>
2026-06-29 12:55:34 -0400

    Update custom.js

--- assets/librerias/js/custom.js
+++ assets/librerias/js/custom.js
@@ -17,6 +17,15 @@
     $('#bd-play-btn').on('click', function () {
         $('#bd-single-hero').addClass('d-none');
         $('#bd-single-video').removeClass('d-none');
-    });
 
+        // Forzar autoplay en iframe
+        var $iframe = $('#video-container').find('iframe');
+        if ($iframe.length) {
+            var src = $iframe.attr('src');
+            if (src.indexOf('autoplay=1') === -1) {
+                var separator = src.indexOf('?') !== -1 ? '&' : '?';
+                $iframe.attr('src', src + separator + 'autoplay=1');
+            }
+        }
+    });
 })(jQuery);
