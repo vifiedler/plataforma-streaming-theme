c3960575bc93135641a6a22ef436b763bd60e820
vifiedler <vi.fiedler@duocuc.cl>
2026-06-29 11:31:51 -0400

    Update content-single.php

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -30,7 +30,7 @@ $generos = get_the_terms(get_the_ID(), 'genero_videos');
                 <?php if (!empty($generos) && !is_wp_error($generos)): ?>
                 <div class="d-flex flex-wrap gap-2">
                     <?php foreach ($generos as $g): ?>
-                    <a href="<?php echo esc_url(get_term_link($g)); ?>" class="bd-tag">
+                    <a href="<?php echo esc_url(get_term_link($g)); ?>" class="bd-tag bd-enlace">
                         <?php echo esc_html($g->name); ?>
                     </a>
                     <?php endforeach; ?>
