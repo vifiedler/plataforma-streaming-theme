e65d122445c5cfd87b3f835656f7629715ae6990
vifiedler <vi.fiedler@duocuc.cl>
2026-06-29 18:04:23 -0400

    Update template-footer.php

--- assets/templates/footers/template-footer.php
+++ assets/templates/footers/template-footer.php
@@ -1,16 +1,12 @@
 <footer id="colophon" class="fbs__footer mt-3">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-12">
                 <?php if (is_active_sidebar('footer_1')):
                     dynamic_sidebar('footer_1');
                 endif; ?>
-            </div>
-            <div class="col-12">
                 <?php if (is_active_sidebar('footer_2')):
                     dynamic_sidebar('footer_2');
                 endif; ?>
-            </div>
         </div>
     </div>
     <!-- Créditos -->
